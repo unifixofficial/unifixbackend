72add0e19b98a7cfbde37f1741f6cbfb155aedeb
unifixofficial <unifixofficial365@gmail.com>
2026-04-28 17:28:57 +0530

    rediss fixed

--- config/redis.js
+++ config/redis.js
@@ -4,13 +4,13 @@ const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
   enableReadyCheck: false,
   retryStrategy: (times) => Math.min(times * 500, 5000),
+  tls: process.env.REDIS_URL?.startsWith('rediss://') ? {} : undefined,
 });
 
 redis.on('connect', () => console.log('[Redis] Connected'));
-redis.on('error', (err) => {
-  if (err.code !== 'ECONNRESET' && err.code !== 'ENOTFOUND') {
-    console.error('[Redis] Error:', err.message);
-  }
-}); 
+redis.on('ready', () => console.log('[Redis] Ready'));
+redis.on('error', (err) => console.error('[Redis] Error:', err.message, err.code));
+redis.on('close', () => console.log('[Redis] Connection closed'));
+redis.on('reconnecting', () => console.log('[Redis] Reconnecting...'));
 
 module.exports = redis;
